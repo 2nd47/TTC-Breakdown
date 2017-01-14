947aa324b295114bcadf4c567bc5a2d47b6bf8bd
Christine Murad <christine.murad@mail.utoronto.ca>
2017-01-14 14:29:40 -0500

    Ball moves from station to station

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public Station oldStation;
     public Station currentStation;
     public int currentTime;
-	// Use this for initialization
-	void Start () {
-        currentTime = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public Transform newTransform;
+
+	public float speed;
+
+	public void moveToStation() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(currentStation.transform.position, oldStation.transform.position, step);
+		transform.position = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
 	}
 }
